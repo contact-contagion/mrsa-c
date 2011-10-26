e0d5c0fa13d6151dddc819bac6fc8510a1054e07
mnorth <mnorth@eb7698ad-c3c5-4393-a60c-5d690fe7bf3d>
2011-10-26 20:57:45 +0000

    Updated code comments.
    git-svn-id: https://scrap.dis.anl.gov/repos/MRSA/models/agentmodel@154 eb7698ad-c3c5-4393-a60c-5d690fe7bf3d

--- MRSA/src/mrsa/relogo/ActivityList.java
+++ MRSA/src/mrsa/relogo/ActivityList.java
@@ -7,11 +7,20 @@ public class ActivityList extends ArrayList<Activity> implements Comparable {
 	
 	// Return case identifier, if defined.
 	public String getTucaseid() {
+		
+		// Check for elements.
 		if (this.size() > 0) {
+			
+			// Compare the case identifier.
 			return this.get(0).tucaseid;
+			
 		} else {
+			
+			// Signal that there is no element to compare.
 			return null;
+			
 		}
+		
 	}
 
 	// The comparator
@@ -20,11 +29,21 @@ public class ActivityList extends ArrayList<Activity> implements Comparable {
 		
 		// Check the object.
 		if ((this.getTucaseid() != null) && (obj instanceof ActivityList)) {
+			
+			// Recast the item to compare.
 			ActivityList otherActivity = (ActivityList) obj;
+			
+			// Compare based on case identifiers.
 			return this.getTucaseid().compareTo(otherActivity.getTucaseid());
+
 		} else {
+			
+			
+			// Signal that there is no valid comparison.
 			return 0;
+
 		}
+
 	}
 	
 }
