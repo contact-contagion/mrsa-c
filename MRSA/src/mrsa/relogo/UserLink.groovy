fd65694bed2590e04e7bdc58ca2a4d546ded7c17
mnorth <mnorth@eb7698ad-c3c5-4393-a60c-5d690fe7bf3d>
2012-01-30 04:16:35 +0000

    Updated to Repast 2.0 Final.
    git-svn-id: https://scrap.dis.anl.gov/repos/MRSA/models/agentmodel@204 eb7698ad-c3c5-4393-a60c-5d690fe7bf3d

--- MRSA/src/mrsa/relogo/UserLink.groovy
+++ MRSA/src/mrsa/relogo/UserLink.groovy
@@ -3,12 +3,7 @@ package mrsa.relogo
 import static repast.simphony.relogo.Utility.*;
 import static repast.simphony.relogo.UtilityG.*;
 import repast.simphony.relogo.BaseLink;
-import repast.simphony.relogo.Directed;
-import repast.simphony.relogo.Plural;
 import repast.simphony.relogo.Stop;
-import repast.simphony.relogo.Undirected;
-import repast.simphony.relogo.Utility;
-import repast.simphony.relogo.UtilityG;
 
 // The user link base class.
 class UserLink extends BaseLink	{
