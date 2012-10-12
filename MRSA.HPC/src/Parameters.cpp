a8e0f15734f8eb5fd1a520653eefc3a6a76d91fe
collier <collier@eb7698ad-c3c5-4393-a60c-5d690fe7bf3d>
2012-10-12 15:04:27 +0000

    Added seek and destroy enabled parameter with explicit method in parameters.
    git-svn-id: https://scrap.dis.anl.gov/repos/MRSA/models/agentmodel@361 eb7698ad-c3c5-4393-a60c-5d690fe7bf3d

--- MRSA.HPC/src/Parameters.cpp
+++ MRSA.HPC/src/Parameters.cpp
@@ -40,24 +40,13 @@ const std::string SEEK_CARE_INFECT_PERIOD = "seek.care.infection.period";
 const std::string SELF_CARE_INFECT_PERIOD = "self.care.infection.period";
 const std::string SEEK_AND_DESTROY_ENABLED = "seek.and.destroy.enabled";
 
- const std::string A = "a";
- const std::string B = "b";
- const std::string E = "e";
- const std::string ALPHA ="alpha";
- const std::string THETA = "theta";
- const std::string GAMMA = "gamma";
- const std::string Z = "z";
-
-
- const std::string A = "a";
- const std::string B = "b";
- const std::string E = "e";
- const std::string ALPHA ="alpha";
- const std::string THETA = "theta";
- const std::string GAMMA = "gamma";
- const std::string Z = "z";
-
-
+const std::string A = "a";
+const std::string B = "b";
+const std::string E = "e";
+const std::string ALPHA ="alpha";
+const std::string THETA = "theta";
+const std::string GAMMA = "gamma";
+const std::string Z = "z";
 
 Parameters* Parameters::instance_ = 0;
 
