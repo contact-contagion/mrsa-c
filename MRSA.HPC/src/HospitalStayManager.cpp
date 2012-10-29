37b0dec9429ba10a0f8b9ecffa7219c64f899a29
collier <collier@eb7698ad-c3c5-4393-a60c-5d690fe7bf3d>
2012-10-29 14:08:02 +0000

    fix: stays initialization for first 5 years.
    git-svn-id: https://scrap.dis.anl.gov/repos/MRSA/models/agentmodel@383 eb7698ad-c3c5-4393-a60c-5d690fe7bf3d

--- MRSA.HPC/src/HospitalStayManager.cpp
+++ MRSA.HPC/src/HospitalStayManager.cpp
@@ -13,19 +13,29 @@
 namespace mrsa {
 
 HospitalStayManager::HospitalStayManager(unsigned int y1_length, unsigned int y2_length,
-		unsigned int y3_length, unsigned int y4_length, unsigned int y5_length) : IHospitalStayManager(),
-				stays(10, std::make_pair(0u, 0u)), duration(0), start_day(0) {
+		unsigned int y3_length, unsigned int y4_length, unsigned int y5_length) :
+		IHospitalStayManager(), stays(10, std::make_pair(0u, 0u)), duration(0), start_day(0) {
 
-	stays.push_back(std::pair<unsigned int, unsigned int>
-			((unsigned int)repast::Random::instance()->createUniIntGenerator(1, 365 - y1_length).next(), y1_length));
-	stays.push_back(std::pair<unsigned int, unsigned int>
-				((unsigned int)repast::Random::instance()->createUniIntGenerator(1, 365 - y2_length).next(), y2_length));
-	stays.push_back(std::pair<unsigned int, unsigned int>
-				((unsigned int)repast::Random::instance()->createUniIntGenerator(1, 365 - y3_length).next(), y3_length));
-	stays.push_back(std::pair<unsigned int, unsigned int>
-				((unsigned int)repast::Random::instance()->createUniIntGenerator(1, 365 - y4_length).next(), y4_length));
-	stays.push_back(std::pair<unsigned int, unsigned int>
-				((unsigned int)repast::Random::instance()->createUniIntGenerator(1, 365 - y5_length).next(), y5_length));
+	stays[0] =
+			(std::pair<unsigned int, unsigned int>(
+					(unsigned int) repast::Random::instance()->createUniIntGenerator(1,
+							365 - y1_length).next(), y1_length));
+	stays[1] =
+			(std::pair<unsigned int, unsigned int>(
+					(unsigned int) repast::Random::instance()->createUniIntGenerator(1,
+							365 - y2_length).next(), y2_length));
+	stays[2] =
+			(std::pair<unsigned int, unsigned int>(
+					(unsigned int) repast::Random::instance()->createUniIntGenerator(1,
+							365 - y3_length).next(), y3_length));
+	stays[3] =
+			(std::pair<unsigned int, unsigned int>(
+					(unsigned int) repast::Random::instance()->createUniIntGenerator(1,
+							365 - y4_length).next(), y4_length));
+	stays[4] =
+			(std::pair<unsigned int, unsigned int>(
+					(unsigned int) repast::Random::instance()->createUniIntGenerator(1,
+							365 - y5_length).next(), y5_length));
 }
 
 HospitalStayManager::~HospitalStayManager() {
@@ -43,7 +53,7 @@ bool HospitalStayManager::inHospital(int year, int day) {
 	} else {
 		// year starts with 1
 		StayData& data = stays.at(year - 1);
-		ret_val = data.second > 0 && data.first == (unsigned int)day;
+		ret_val = data.second > 0 && data.first == (unsigned int) day;
 		if (ret_val) {
 			start_day = day;
 			duration = data.second;
